a6a451ff590a68d308e5df4ee39936972c0896d9
juhi-developer <developer.juhi@gmail.com>
2025-05-09 22:40:54 +0530

    sss

--- e2e/globalSetup.js
+++
@@ -1,9 +0,0 @@
-const {setup: detoxSetup} = require('detox');
-const config = require('../.detoxrc.js'); // Adjust the path as necessary
-
-module.exports = async () => {
-  await detoxSetup(config);
-
-  // Set a global variable to indicate test mode
-  global.__IS_TEST_MODE__ = true;
-};
--- ios/AppStateListener.h
+++
@@ -1,7 +0,0 @@
-// AppStateListener.h
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
-@interface AppStateListener : RCTEventEmitter <RCTBridgeModule>
-
-@end
